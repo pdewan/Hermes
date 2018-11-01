1eb5311a00bd96412aa52031dd92cfe17f951ec8
pdewan <dewan@cs.unc.edu>
2018-11-01 13:54:18 -0400

    Changed to direct time stamp computer

--- Hermes/src/main/java/analyzer/TimeStampComputerFactory.java
+++ Hermes/src/main/java/analyzer/TimeStampComputerFactory.java
@@ -1,7 +1,9 @@
 package analyzer;
 
 public class TimeStampComputerFactory {
-	static TimeStampComputer singleton =  new AResettingTimeStampComputer();
+//	static TimeStampComputer singleton =  new AResettingTimeStampComputer();
+	static TimeStampComputer singleton =  new ADirectTimeStampComputer();
+
 	public static TimeStampComputer getSingleton() {
 		if (singleton == null) {
 			singleton = new AResettingTimeStampComputer();
